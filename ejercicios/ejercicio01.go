14863a502d6c3025afeebedb02c85198099d25fd
HolgadoJairoDavid <jairodavidholgado@gmail.com>
2023-08-25 09:24:25 -0300

    Primer ejercicio resuelto

--- ejercicios/ejercicio01.go
+++ ejercicios/ejercicio01.go
@@ -3,8 +3,10 @@ package ejercicios
 import "strconv"
 
 func PrimerEjercicio(str string) (int, string) {
-	number, _ := strconv.Atoi(str)
-
+	number, err := strconv.Atoi(str)
+	if err != nil {
+		return 0, "Hubo un error " + err.Error()
+	}
 	if number > 100 {
 		return number, "Es mayor a 100"
 	} else {
