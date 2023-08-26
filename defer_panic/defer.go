e20d2b326a8d776b2db4f0cf3ab0415a444d2f90
HolgadoJairoDavid <jairodavidholgado@gmail.com>
2023-08-26 03:51:31 -0300

    mapas, arrays y slice

--- defer_panic/defer.go
+++ defer_panic/defer.go
@@ -2,10 +2,24 @@ package defer_panic
 
 import (
 	"fmt"
+	"log"
 )
 
 func VemosDefer() {
 	fmt.Println("Este es un primer mensaje")
 	defer fmt.Println("Este es el mensaje final")
 	fmt.Println("Este es un segundo mensaje")
+}
+
+func EjemploPanic() {
+	defer func() {
+		reco := recover()
+		if reco != nil {
+			log.Fatalf("ocurrió un error que generó Panic \n %v", reco)
+		}
+	}()
+	a := 1
+	if a == 1 {
+		panic("a es igual a 1")
+	}
 }
